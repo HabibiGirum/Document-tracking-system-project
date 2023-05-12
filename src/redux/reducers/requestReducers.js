117b9bdc3689124bba592531863fac7964997c01
Abrham Lakew <ablakew7ab@gmail.com>
2023-05-12 14:33:10 -0500

    request reducers added

--- src/redux/reducers/requestReducers.js
+++ src/redux/reducers/requestReducers.js
@@ -12,3 +12,59 @@ import {
   OPEN_FILE,
   CLEAR_FILTERS,
 } from "../constants/requestConstants";
+
+const initialState = {
+  isLoading: false,
+  requests: [],
+  totalRequests: 0,
+  numOfPages: 0,
+  editRequestId: null,
+  error: null,
+};
+
+const requestReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_REQUEST_BEGIN:
+    case GET_REQUESTS_BEGIN:
+    case DELETE_REQUEST_BEGIN:
+    case EDIT_REQUEST_BEGIN:
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+    case CREATE_REQUEST_SUCCESS:
+    case GET_REQUESTS_SUCCESS:
+    case EDIT_REQUEST_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        error: null,
+        requests: action.payload.requests,
+        totalRequests: action.payload.totalRequests,
+        numOfPages: action.payload.numOfPages,
+      };
+    case CREATE_REQUEST_ERROR:
+    case EDIT_REQUEST_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.msg,
+      };
+    case SET_EDIT_REQUEST:
+      return {
+        ...state,
+        editRequestId: action.payload.id,
+      };
+    case OPEN_FILE:
+    case CLEAR_FILTERS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export default requestReducer;
