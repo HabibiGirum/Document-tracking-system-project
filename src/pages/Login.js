0888e1883d5f06df1958ce6b29c77a42d7f75e77
Habtamu Girum <habtamugirum478@gmail.com>
2023-05-15 20:36:23 +0300

    DTS final

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,49 +25,6 @@ const Login = () => {
   return (
     <Row>
     <Col>
-    <Container>
-    <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh"}}
-      >
-
-                
-                    <Card>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="home">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
-
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-
-                            <Form.Select aria-label="Default select example">
-                                <Form.Label>select document type</Form.Label>
-                                <option>select document type</option>
-                                <option value="1">Leave</option>
-                                <option value="2">Requritment</option>
-                                <option value="3">Promostion</option>
-                            </Form.Select>
-                            <Form.Group controlId="formFileMultiple" className="mb-3">
-                                <Form.Label>Multiple files input example</Form.Label>
-                                <Form.Control type="file" multiple />
-                            </Form.Group>
-                            <Button variant="secondary" type="submit">
-                                Submit
-                            </Button>
-                        </Form>
-                    </Card>
-                
-                </div>
-            </Container>
-    </Col>
-    <Col>
       <div
         className="d-flex justify-content-center align-items-center md"
         style={{ height: "100vh"}}
