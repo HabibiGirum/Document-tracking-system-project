967c106e7fd372f2521bf502e3c9b7bf0cac019f
Abrham Lakew <ablakew7ab@gmail.com>
2023-05-18 04:08:32 -0500

    add sorting method on the recieved new page

--- src/pages/RecievedNew.js
+++ src/pages/RecievedNew.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getRequests, openFile } from "../redux/actions/requestAction";
 import { Table } from "react-bootstrap";
@@ -6,10 +6,16 @@ import { Table } from "react-bootstrap";
 const ReceivedPage = () => {
   const dispatch = useDispatch();
   const { requests, isLoading, error } = useSelector((state) => state.requests);
+  const [sortedRequests, setSortedRequests] = useState([]);
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     dispatch(getRequests());
   }, [dispatch]);
+
+  useEffect(() => {
+    setSortedRequests([...requests]);
+  }, [requests]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -19,10 +25,25 @@ const ReceivedPage = () => {
     return <div>Error: {error}</div>;
   }
 
-  console.log(requests);
-
   const handleFileOpen = (filename) => {
     dispatch(openFile(filename));
+  };
+
+  const handleSort = (key) => {
+    if (key === "createdAt") {
+      // Sort by createdAt
+      const sorted = [...sortedRequests].sort((a, b) =>
+        sortOrder === "asc"
+          ? a.createdAt.localeCompare(b.createdAt)
+          : b.createdAt.localeCompare(a.createdAt)
+      );
+      setSortedRequests(sorted);
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    }
+  };
+
+  const formatDateTime = (dateTime) => {
+    return new Date(dateTime).toLocaleString();
   };
 
   return (
@@ -36,25 +57,32 @@ const ReceivedPage = () => {
             <th>Document Type</th>
             <th>Purpose</th>
             <th>To</th>
+            <th>
+              <button onClick={() => handleSort("createdAt")}>
+                Created Date {sortOrder === "asc" ? "▲" : "▼"}
+              </button>
+            </th>
+            <th>Created Time</th>
             <th>Filename</th>
           </tr>
         </thead>
         <tbody>
-          {requests &&
-            requests.map((request) => (
-              <tr key={request._id}>
-                <td>{request.from}</td>
-                <td>{request.by}</td>
-                <td>{request.documentType}</td>
-                <td>{request.purpose}</td>
-                <td>{request.to}</td>
-                <td>
-                  <button onClick={() => handleFileOpen(request.filename)}>
-                    {request.filename}
-                  </button>
-                </td>
-              </tr>
-            ))}
+          {sortedRequests.map((request) => (
+            <tr key={request._id}>
+              <td>{request.from}</td>
+              <td>{request.by}</td>
+              <td>{request.DocumentType}</td>
+              <td>{request.purpose}</td>
+              <td>{request.to}</td>
+              <td>{formatDateTime(request.createdAt).split(",")[0]}</td>
+              <td>{formatDateTime(request.createdAt).split(",")[1]}</td>
+              <td>
+                <button onClick={() => handleFileOpen(request.filename)}>
+                  {request.filename}
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
