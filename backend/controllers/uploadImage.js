981b12132eb70b5bba752427a1b3c07242d9dc1c
Habtamu Girum <habtamugirum478@gmail.com>
2023-06-08 14:46:57 +0300

    Frontend and Backend

--- backend/controllers/uploadImage.js
+++ backend/controllers/uploadImage.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const { createWorker } = require('tesseract.js');
 
 const storage = multer.diskStorage({
   destination: 'ImageUploads/',
@@ -10,7 +11,11 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage }).single('image');
 
-const uploadImage = (req, res) => {
+const worker = createWorker({
+  logger: (m) => console.log(m),
+});
+
+const uploadImage = async (req, res) => {
   if (!req.file) {
     return res.status(400).json({ error: 'No image provided' });
   }
@@ -18,7 +23,24 @@ const uploadImage = (req, res) => {
   // Process the uploaded file as needed
   // For example, you can move the file to a different location using fs or process it in some way
 
-  res.status(200).json({ message: 'Image uploaded successfully' });
+  const imagePath = req.file.path;
+
+  try {
+   await ( await worker).load();
+    await (await worker).loadLanguage('eng');
+    await (await worker).initialize('eng');
+    await (await worker).setParameters({
+      tessedit_char_whitelist: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+    });
+    const { data } = await (await worker).recognize(imagePath);
+    console.log(data.text);
+    await (await worker).terminate();
+
+    res.status(200).json({ message: 'Image uploaded successfully', extractedText: data.text });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ message: 'Failed to extract text from image' });
+  }
 };
 
 module.exports = {
